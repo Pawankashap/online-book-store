9703db5ae0e78e5d1ba973b75b9c6424bbddd623
Pawankashap <pawan.3940@gmail.com>
2023-10-02 21:29:58 -0700

    updates route elements

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,42 @@
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import Header from "./Header";
-import BirdPage from "./BirdPage";
-import BirdDetail from "./BirdDetail"
+import NavBar from "./NavBar";
+import Login from "../pages/Login";
+import BooksList from "../pages/BooksList";
+import Books from "../pages/Books";
 
 function App() {
+  const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    // auto-login
+    fetch("/check_session").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
 
-    return (
-        <>
-          <main>
-            <Header />
-            <Routes>
-              <Route path="/:id" element={ <BirdDetail /> }/>
-              <Route path="/" element={ <BirdPage /> }/>
-            </Routes>
-          </main>
-        </>
-      );
+  if (!user) return <Login onLogin={setUser} />;
+
+  return (
+    <>
+      <NavBar user={user} setUser={setUser} />
+      <main>
+        <Routes>
+        
+            {/* <Route path="/new">
+              <Books user={user} />
+            </Route> */}
+            <Route path="/new" element={<Books />} />
+            {/* <Route path="/">
+              <BooksList />
+            </Route> */}
+            <Route path="/" element={<BooksList />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
