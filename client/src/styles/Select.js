4d3a3cc34d12b0c072dbeba9cd9f596b8d081368
Pawankashap <pawan.3940@gmail.com>
2023-10-05 23:49:31 -0700

    add select style

---
+++ client/src/styles/Select.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Select = styled.select`
+  width: 100%;
+  padding: 0.5rem;
+  font-size: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  outline: none;
+
+  &:focus {
+    border-color: #007bff;
+  }
+`;
+
+
+export default Select;
