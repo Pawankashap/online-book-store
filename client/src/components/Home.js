862bced857436108df2c3bd0b6aa0e13a2daf2f5
Pawankashap <pawan.3940@gmail.com>
2023-10-05 23:50:05 -0700

    adds Home menu

---
+++ client/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import bannerImage from "../Images/bgimage.jpg"
+
+function Home({user}) {
+  return (
+    <div>
+      <h1>Welcome to Online Book Store</h1>
+      <img src={bannerImage} alt="Banner" style={{ width: "100%", height: "auto" }} />
+      {/* Add other content */}
+    </div>
+  );
+}
+
+export default Home;
